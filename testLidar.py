c3936a7b8091e6e6492f55c4f086d0b1512ced95
Brian Aditya <brianadit33@gmail.com>
2023-08-12 23:33:42 +0700

    fix

--- testLidar.py
+++ testLidar.py
@@ -7,7 +7,7 @@ vehicle = connect('localhost:14550', baud=57600, wait_ready=False)
 
 # Increase altitude
 while True:
-  current_altitude = vehicle.rangefinder[bottom_rangefinder].distance
+  current_altitude = vehicle.rangefinder.distance
   print("Altitude: %f"%current_altitude*1.0)
   if current_altitude >= 1:
     print("Reached target altitude")
