e24a1a0e494aad4aefd5200b86ee986a040ef7f3
Brian Aditya <brianadit33@gmail.com>
2023-08-13 22:36:10 +0700

    fix test

--- testMagnet.py
+++ testMagnet.py
@@ -1,6 +1,12 @@
 import Jetson.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(29, GPIO.OUT)
 
+# set high for 10 seconds
 GPIO.output(29, GPIO.HIGH)
+time.sleep(10)
+
+# set low
+GPIO.output(29, GPIO.LOW)
