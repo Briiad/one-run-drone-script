d2508cc01ac1c74fbae1cffc463fa830d179ad95
Brian Aditya <brianadit33@gmail.com>
2023-08-15 02:37:35 +0700

    fix test

--- testcam.py
+++ testcam.py
@@ -27,37 +27,37 @@ net.SetTrackingParams(minFrames=3, dropFrames=15, overlapThreshold=0.5)
 
 while True:
     res, frame = cam_1.read()
-    resized = cv2.resize(frame, (640, 480))
+    resized = cv2.resize(resized, (640, 480))
 
     if not res:
-        print("Ignoring empty camera frame.")
+        print("Ignoring empty camera resized.")
         continue
 
     # Detect objects
-    img = cudaFromNumpy(frame)
+    img = cudaFromNumpy(resized)
     detections = net.Detect(img)
 
-    # line to draw in the middle of the frame ( +10 and -10 to make sure it's in the middle )
-    cv2.line(frame, (int(frame.shape[1] / 2) - 200, 0), (int(frame.shape[1] / 2) - 200, frame.shape[0]), (0, 255, 0), 2)
-    cv2.line(frame, (int(frame.shape[1] / 2) + 200, 0), (int(frame.shape[1] / 2) + 200, frame.shape[0]), (0, 255, 0), 2)
+    # line to draw in the middle of the resized ( +10 and -10 to make sure it's in the middle )
+    cv2.line(resized, (int(resized.shape[1] / 2) - 200, 0), (int(resized.shape[1] / 2) - 200, resized.shape[0]), (0, 255, 0), 2)
+    cv2.line(resized, (int(resized.shape[1] / 2) + 200, 0), (int(resized.shape[1] / 2) + 200, resized.shape[0]), (0, 255, 0), 2)
 
     for detection in detections:
         # centroid coordinates
         x = int(detection.Center[0])
         y = int(detection.Center[1])
         # draw circle on centroid
-        cv2.circle(frame, (x, y), 5, (0, 0, 255), -1)
+        cv2.circle(resized, (x, y), 5, (0, 0, 255), -1)
         # draw bounding box
-        cv2.rectangle(frame, (int(detection.Left), int(detection.Top)), (int(detection.Right), int(detection.Bottom)), (255, 0, 0), 2)
+        cv2.rectangle(resized, (int(detection.Left), int(detection.Top)), (int(detection.Right), int(detection.Bottom)), (255, 0, 0), 2)
         # object class name and confidence
-        cv2.putText(frame, "%s (%.1f%%)" % (detection.ClassID, detection.Confidence * 100), (int(detection.Left), int(detection.Top) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-        # distance between centroid and middle of the frame
-        distance = math.sqrt((detection.Center[0] - int(frame.shape[1] / 2))**2 + (detection.Center[1] - int(frame.shape[0] / 2))**2)
+        cv2.putText(resized, "%s (%.1f%%)" % (detection.ClassID, detection.Confidence * 100), (int(detection.Left), int(detection.Top) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        # distance between centroid and middle of the resized
+        distance = math.sqrt((detection.Center[0] - int(resized.shape[1] / 2))**2 + (detection.Center[1] - int(resized.shape[0] / 2))**2)
         # draw distance
-        cv2.putText(frame, "Distance: %.1f" % distance, (int(detection.Left), int(detection.Top) - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+        cv2.putText(resized, "Distance: %.1f" % distance, (int(detection.Left), int(detection.Top) - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
         print("Distance: %.1f" % distance)
         # if centroid coordinates is in the middlle of the line, drop payload
-        if x >= int(frame.shape[1] / 2) + 200 and x <= int(frame.shape[1] / 2) - 200:
+        if x >= int(resized.shape[1] / 2) + 200 and x <= int(resized.shape[1] / 2) - 200:
             # move forward a bit
             print("Moving forward a bit")
             time.sleep(1)
@@ -69,14 +69,14 @@ while True:
                 break
             # if centroid outside the line, adjust position
         else:
-            # if centroid coordinates is in the right side of the frame, roll right
-            if x >= int(frame.shape[1] / 2) + 200:
+            # if centroid coordinates is in the right side of the resized, roll right
+            if x >= int(resized.shape[1] / 2) + 200:
                 print("Roll right")
-            # if centroid coordinates is in the left side of the frame, roll left
-            elif x <= int(frame.shape[1] / 2) - 200:
+            # if centroid coordinates is in the left side of the resized, roll left
+            elif x <= int(resized.shape[1] / 2) - 200:
                 print("Roll left")
 
-    cv2.imshow("Camera 1", frame)
+    cv2.imshow("Camera 1", resized)
 
     if cv2.waitKey(1) == ord('q'):
         print("Quitting...")
